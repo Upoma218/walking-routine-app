2ba1ca05a4a70b9345ca9f6d014f71aab204e953
Resma Islam Upoma <upoma.cdsr@gmail.com>
2022-09-29 07:57:08 +0600

    Questions Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Activities from './Components/Activities/Activities';
+import Questions from './Components/Questions/Questions';
 
 function App() {
   return (
     <div className="App">
      <Header></Header>
      <Activities></Activities>
+     <Questions></Questions>
     </div>
   );
 }
---
+++ src/Components/Questions/Questions.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./Questions.css"
+
+const Questions = () => {
+    return (
+        <div className='questions'>
+            <h1>How Does React Work?</h1>
+            <p></p>
+            <h1>What is The Difference Between Props an State?</h1>
+            <p></p>
+            <h1>How Many Tasks We Can Do By Using "useEffect" Without Loading Data From API? </h1>
+            <p></p>
+        </div>
+    );
+};
+
+export default Questions;
